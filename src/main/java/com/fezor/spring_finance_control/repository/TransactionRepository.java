aadbe85870498572246f4a5a9f2f43f10efc9e84
felipezzz1 <felipezorzo78@gmail.com>
2026-02-04 23:02:46 -0300

    fix - findByDateBetween

--- src/main/java/com/fezor/spring_finance_control/repository/TransactionRepository.java
+++ src/main/java/com/fezor/spring_finance_control/repository/TransactionRepository.java
@@ -3,17 +3,14 @@ package com.fezor.spring_finance_control.repository;
 import com.fezor.spring_finance_control.model.Transaction;
 import com.fezor.spring_finance_control.model.TransactionType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findDateBetween(LocalDate start, LocalDate end);
+    List<Transaction> findByDateBetween(LocalDate start, LocalDate end);
 
     List<Transaction> findByType(TransactionType type);
 
