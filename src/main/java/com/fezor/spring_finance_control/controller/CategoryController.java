94efc887cf4d192981d2a4d4979d50316243d046
felipezzz1 <felipezorzo78@gmail.com>
2026-02-04 22:50:55 -0300

    feat - categoryController CRUD

--- src/main/java/com/fezor/spring_finance_control/controller/CategoryController.java
+++ src/main/java/com/fezor/spring_finance_control/controller/CategoryController.java
@@ -25,4 +25,18 @@ public class CategoryController {
     public List<Category> findAll(){
         return service.findAll();
     }
+
+    @GetMapping("/{id}")
+    public Category findById(@PathVariable Long id) { return service.findById(id);}
+
+    @PutMapping("/{id}")
+    public Category update(@PathVariable Long id, @RequestBody Category category) {
+        return service.update(id, category);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
 }
