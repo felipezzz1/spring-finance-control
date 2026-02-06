a6e604bc0c134fe069cc71cef36542d92640ad32
felipezzz1 <felipezorzo78@gmail.com>
2026-02-05 23:23:12 -0300

    feat: create CategoryResponse

---
+++ src/main/java/com/fezor/spring_finance_control/dto/CategoryResponse.java
@@ -0,0 +1,10 @@
+package com.fezor.spring_finance_control.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CategoryResponse {
+    private Long id;
+    private String name;
+}
