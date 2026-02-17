5138933daa5334c49c74a8800cee06ca76688bd7
felipezzz1 <felipezorzo78@gmail.com>
2026-02-17 11:34:38 -0300

    fix: add 401 when there isn't a token

--- src/main/java/com/fezor/spring_finance_control/security/config/SecurityConfig.java
+++ src/main/java/com/fezor/spring_finance_control/security/config/SecurityConfig.java
@@ -1,21 +1,33 @@
-package com.fezor.spring_finance_control.config;
+package com.fezor.spring_finance_control.security.config;
 
+import com.fezor.spring_finance_control.security.handler.JwtAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationEntryPoint authEntryPoint;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
                 .csrf(csrf -> csrf.disable())
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(authEntryPoint)
+                )
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
                 ).build();
     }
 
